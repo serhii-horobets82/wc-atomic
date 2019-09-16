7e125f3b664e8636a0fcafdba747789d425c015f
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2019-09-16 11:17:59 +0200

    Init

--- source/organisms/flex-container/component.ts
+++ source/organisms/flex-container/component.ts
@@ -10,196 +10,196 @@ import {FlexContainerInputData} from "./model";
 const componentCSS = require('./component.css');
 
 @customElement('component-flex-container')
-export class FlexComponent extends AbstractComponent<
-   FlexContainerInputData,
-   undefined
-> {
-   static styles = css`
+export class FlexComponent extends AbstractComponent<FlexContainerInputData,
+    undefined> {
+    static styles = css`
       ${unsafeCSS(componentCSS)}
    `;
 
-   static IDENTIFIER: string = 'FlexComponent';
+    static IDENTIFIER: string = 'FlexComponent';
 
-   @property()
-   private _componentsInputData: AbstractInputData[];
+    @property()
+    private _componentsInputData: AbstractInputData[] = [];
 
-   constructor() {
-      super();
-   }
+    constructor() {
+        super();
+    }
 
-   get componentsInputData(): AbstractInputData[] {
-      return this._componentsInputData !== undefined
-         ? this._componentsInputData
-         : [];
-   }
+    get componentsInputData(): AbstractInputData[] {
+        return this._componentsInputData !== undefined
+            ? this._componentsInputData
+            : [];
+    }
 
-   set componentsInputData(value: AbstractInputData[]) {
-      this._componentsInputData = value;
-   }
+    set componentsInputData(value: AbstractInputData[]) {
+        this._componentsInputData = value;
+    }
 
-   @property()
-   private _componentsMap = new Map<
-      AbstractInputData,
-      AbstractComponent<any, any>
-   >();
+    @property()
+    private _componentsMap = new Map<AbstractInputData,
+        AbstractComponent<any, any>>();
 
-   get componentsMap(): Map<AbstractInputData, AbstractComponent<any, any>> {
-      return this._componentsMap;
-   }
+    get componentsMap(): Map<AbstractInputData, AbstractComponent<any, any>> {
+        return this._componentsMap;
+    }
 
-   set componentsMap(
-      value: Map<AbstractInputData, AbstractComponent<any, any>>
-   ) {
-      this._componentsMap = value;
-   }
+    set componentsMap(
+        value: Map<AbstractInputData, AbstractComponent<any, any>>
+    ) {
+        this._componentsMap = value;
+    }
 
-   protected createComponentFromData(
-      componentInputData: AbstractInputData
-   ): AbstractComponent<any, any> {
-      console.log(
-         'create component from inputData= ' +
+    protected createComponentFromData(
+        componentInputData: AbstractInputData
+    ): AbstractComponent<any, any> {
+        console.log(
+            'create component from inputData= ' +
             componentInputData.componentIdentifier
-      );
-      let component = this._componentsMap.get(componentInputData);
-      if (component == null) {
-         component = ComponentLoader.INSTANCE.createComponentFromInputData(
-            componentInputData
-         );
-         this._componentsMap.set(componentInputData, component);
-      }
-      console.log(
-         'component created: ' + componentInputData.componentIdentifier
-      );
-      return component;
-   }
+        );
+        let component = this._componentsMap.get(componentInputData);
+        if (component == null) {
+            component = ComponentLoader.INSTANCE.createComponentFromInputData(
+                componentInputData
+            );
+            this._componentsMap.set(componentInputData, component);
+        }
+        console.log(
+            'component created: ' + componentInputData.componentIdentifier
+        );
+        return component;
+    }
 
-   @property()
-   gridClazz: string = 'grid_100';
+    @property()
+    gridClazz: string = 'grid_100';
 
-   @property()
-   itemClazz: string = '';
+    @property()
+    itemClazz: string = '';
 
-   @property()
-   columnFlexBasisValues: string[] = [];
+    @property()
+    columnFlexBasisValues: string[] = [];
 
-   @property()
-   columnFlexBasisValue: string = 'auto';
+    @property()
+    columnFlexBasisValue: string = 'auto';
 
-   @query('#slotElement')
-   slotElement: HTMLSlotElement;
+    @query('#slotElement')
+    slotElement: HTMLSlotElement | undefined;
 
-   render() {
-      return html`
+    render() {
+        return html`
          <div class="grid ${this.gridClazz}">
             ${guard(
-               this.componentsInputData,
-               () =>
-                  html`
+            this.componentsInputData,
+            () =>
+                html`
                      ${repeat(
-                        this.componentsInputData,
-                        (componentInputData, index) => html`
+                    this.componentsInputData,
+                    (componentInputData, index) => html`
                            <div
                               class="grid_content ${this.itemClazz}"
                               style="${this.getFlexBasis(index)};"
                            >
                               ${this.createComponentFromData(
-                                 componentInputData
-                              )}
+                        componentInputData
+                    )}
                            </div>
                         `
-                     )}
+                )}
                   `
-            )}
+        )}
             <slot id="slotElement"></slot>
          </div>
       `;
-   }
+    }
 
-   getFlexBasis(index: number): string {
-      let flexBasisValue =
-         this.columnFlexBasisValues !== undefined
-            ? this.columnFlexBasisValues[index] !== undefined
-               ? this.columnFlexBasisValues[index]
-               : this.columnFlexBasisValue
-            : undefined;
-      return 'flex-basis: ' + flexBasisValue + ';max-width: ' + flexBasisValue;
-   }
+    getFlexBasis(index: number): string {
+        let flexBasisValue =
+            this.columnFlexBasisValues !== undefined
+                ? this.columnFlexBasisValues[index] !== undefined
+                ? this.columnFlexBasisValues[index]
+                : this.columnFlexBasisValue
+                : undefined;
+        return 'flex-basis: ' + flexBasisValue + ';max-width: ' + flexBasisValue;
+    }
 
-   updated(_changedProperties: Map<PropertyKey, unknown>): void {
-      if (this.shadowRoot === null) {
-         console.debug('shadow root not active');
-         return;
-      }
+    updated(_changedProperties: Map<PropertyKey, unknown>): void {
+        if (this.shadowRoot === null) {
+            console.debug('shadow root not active');
+            return;
+        }
 
-      let elements: Element[] = this.slotElement.assignedElements();
+        if (this.slotElement == null) {
+            return;
+        }
 
-      for (let index = 0; index < elements.length; index++) {
-         let element: Element = elements[index];
+        let elements: Element[] = this.slotElement.assignedElements();
 
-         let classList = element.classList;
-         if (!classList.contains('grid_content')) {
-            classList.add('grid_content');
-         }
-         if (this.itemClazz.length > 0 && !classList.contains(this.itemClazz)) {
-            classList.add(this.itemClazz);
-         }
+        for (let index = 0; index < elements.length; index++) {
+            let element: Element = elements[index];
 
-         let currentStyle: string | null = element.getAttribute('style');
-         if (currentStyle === null) {
-            element.setAttribute('style', this.getFlexBasis(index) + ';');
-         } else {
-            let currentStyles = currentStyle.split(';');
-            currentStyle = '';
-            currentStyles.forEach((value) => {
-               if (value.length > 0 && value.lastIndexOf('flex-basis') === -1 && value.lastIndexOf('max-width') === -1) {
-                  currentStyle += value + ';';
-               }
-            });
-            currentStyle += this.getFlexBasis(index) + ';';
-            element.setAttribute('style', currentStyle);
-         }
+            let classList = element.classList;
+            if (!classList.contains('grid_content')) {
+                classList.add('grid_content');
+            }
+            if (this.itemClazz.length > 0 && !classList.contains(this.itemClazz)) {
+                classList.add(this.itemClazz);
+            }
 
-      }
-   }
+            let currentStyle: string | null = element.getAttribute('style');
+            if (currentStyle === null) {
+                element.setAttribute('style', this.getFlexBasis(index) + ';');
+            } else {
+                let currentStyles = currentStyle.split(';');
+                currentStyle = '';
+                currentStyles.forEach((value) => {
+                    if (value.length > 0 && value.lastIndexOf('flex-basis') === -1 && value.lastIndexOf('max-width') === -1) {
+                        currentStyle += value + ';';
+                    }
+                });
+                currentStyle += this.getFlexBasis(index) + ';';
+                element.setAttribute('style', currentStyle);
+            }
 
-   inputDataChanged() {
-      this.componentsInputData =
-         this.inputData.componentsInputData !== undefined
-            ? this.inputData.componentsInputData
-            : [];
+        }
+    }
 
-      this.gridClazz =
-         this.inputData.gridClazz !== undefined
-            ? this.inputData.gridClazz
-            : 'grid_100';
+    inputDataChanged() {
+        this.componentsInputData =
+            this.inputData.componentsInputData !== undefined
+                ? this.inputData.componentsInputData
+                : [];
 
-      this.itemClazz =
-         this.inputData.itemClazz !== undefined ? this.inputData.itemClazz : '';
+        this.gridClazz =
+            this.inputData.gridClazz !== undefined
+                ? this.inputData.gridClazz
+                : 'grid_100';
 
-      this.columnFlexBasisValue =
-         this.inputData.columnFlexBasisValue !== undefined
-            ? this.inputData.columnFlexBasisValue
-            : 'auto';
+        this.itemClazz =
+            this.inputData.itemClazz !== undefined ? this.inputData.itemClazz : '';
 
-      this.columnFlexBasisValues =
-         this.inputData.columnFlexBasisValues !== undefined
-            ? this.inputData.columnFlexBasisValues
-            : [];
-   }
+        this.columnFlexBasisValue =
+            this.inputData.columnFlexBasisValue !== undefined
+                ? this.inputData.columnFlexBasisValue
+                : 'auto';
 
-   getOutputData(): undefined {
-      return undefined;
-   }
+        this.columnFlexBasisValues =
+            this.inputData.columnFlexBasisValues !== undefined
+                ? this.inputData.columnFlexBasisValues
+                : [];
+    }
 
-   getDefaultInputData(): FlexContainerInputData {
-      return <FlexContainerInputData>{
-         componentIdentifier: FlexComponent.IDENTIFIER,
-         gridClazz: 'grid_100',
-         columnFlexBasisValues: ['50%', '50%'],
-         componentsInputData: [
-            new ImgComponent().getDefaultInputData(),
-            new ImgComponent().getDefaultInputData()
-         ]
-      };
-   }
+    getOutputData(): undefined {
+        return undefined;
+    }
+
+    getDefaultInputData(): FlexContainerInputData {
+        return <FlexContainerInputData>{
+            componentIdentifier: FlexComponent.IDENTIFIER,
+            gridClazz: 'grid_100',
+            columnFlexBasisValues: ['50%', '50%'],
+            componentsInputData: [
+                new ImgComponent().getDefaultInputData(),
+                new ImgComponent().getDefaultInputData()
+            ]
+        };
+    }
 }
