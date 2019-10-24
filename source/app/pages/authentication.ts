62887ec49f6fd9d98dceb85f171227aac5340392
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2019-10-24 15:29:43 +0200

    Material Design Anfang

--- source/app/pages/authentication.ts
+++ source/app/pages/authentication.ts
@@ -1,7 +1,7 @@
 import {customElement, html, TemplateResult} from 'lit-element';
 import {BlankTemplate} from "../../templates/blank/template";
 import {router} from "../../util/router";
-import {BalanceOverview, HTTP_CLIENT, Konzern, User} from "../data/data";
+import {HTTP_CLIENT, Konzern, User} from "../data/data";
 import {BALCO_DATA_STORE} from "../data/balco_data";
 
 
@@ -14,17 +14,12 @@ export class AuthenticationPage extends BlankTemplate {
     }
 
     getContent(): TemplateResult {
-        return html` 
- 
- <component-flex-container gridClazz="grid_100 maxPadding"  columnFlexBasisValue="100%;">
- 
- <component-img clazz="imageWidthHundred" src="http://v220190910399797452.supersrv.de/img/Logo_REMONDIS_Claim_E_RGB_72dp.jpg"></component-img>
-
- 
- <component-authentication @component-authentication-event-success="${() => this.successfullyLoggedIn()}" @component-authentication-event-logout="${() => this.successfullyLoggedOut()}"></component-authentication>
-
-</component-flex-container>
-
+        return html`
+             <component-spacer clazz="maxPaddingTop"></component-spacer>  
+             <component-flex-container gridClazz="grid_25"  columnFlexBasisValue="100%;">
+                 <component-img clazz="imageWidthHundred" src="${BALCO_DATA_STORE.IMG_RESOURCE_URL + 'Logo_REMONDIS_Claim_E_RGB_72dpi.jpg'}"></component-img>
+                 <component-authentication @component-authentication-event-success="${() => this.successfullyLoggedIn()}" @component-authentication-event-logout="${() => this.successfullyLoggedOut()}"></component-authentication>
+            </component-flex-container>
 `;
     }
 
