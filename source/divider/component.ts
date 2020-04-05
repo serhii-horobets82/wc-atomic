dac3fc95c8eebc96a41eeec3fbab9c4cb28cf2a0
dominik.bruhn <dominikbruhn@googlemail.com>
2020-04-05 16:34:25 +0200

    modified

--- source/divider/component.ts
+++ source/divider/component.ts
@@ -1,43 +1,43 @@
-import {css, customElement, html, property, unsafeCSS} from 'lit-element';
-import {AbstractComponent, AbstractInputData} from '../abstract-component/component';
-import {SpacerAlignment, SpacerSize} from '..';
+import { css, customElement, html, property, unsafeCSS } from 'lit-element';
+import { AbstractComponent, AbstractInputData } from '../abstract-component/component';
+import { SpacerAlignment, SpacerSize } from '..';
 import { BasicService } from '@domoskanonos/frontend-basis';
 
 const componentCSS = require('./component.css');
 
 export class DividerInputData extends AbstractInputData {
-    componentIdentifier = DividerComponent.IDENTIFIER;
-    spacerSize: SpacerSize = SpacerSize.MEDIUM;
+   componentIdentifier = DividerComponent.IDENTIFIER;
+   spacerSize: SpacerSize = SpacerSize.MEDIUM;
 }
 
 @customElement('component-divider')
 export class DividerComponent extends AbstractComponent<DividerInputData, undefined> {
-    static styles = css`
+   static styles = css`
       ${unsafeCSS(componentCSS)}
    `;
 
-    static IDENTIFIER: string = 'DividerComponent';
+   static IDENTIFIER: string = 'DividerComponent';
 
-    @property()
-    spacerSize: SpacerSize = SpacerSize.MEDIUM;
+   @property()
+   spacerSize: SpacerSize = SpacerSize.MEDIUM;
 
-    render() {
-        return html`
+   render() {
+      return html`
          <component-spacer spacerSize="${this.spacerSize}" spacerAlignment="${SpacerAlignment.BOTH}"> </component-spacer>
          <div class="basicBorder basicBorderBottom"></div>
          <component-spacer spacerSize="${this.spacerSize}" spacerAlignment="${SpacerAlignment.BOTH}"> </component-spacer>
       `;
-    }
+   }
 
-    getDefaultInputData(): any {
-        return <DividerInputData>{};
-    }
+   getDefaultInputData(): any {
+      return <DividerInputData>{};
+   }
 
-    getOutputData(): undefined {
-        return undefined;
-    }
+   getOutputData(): undefined {
+      return undefined;
+   }
 
-    protected inputDataChanged() {
-        this.spacerSize = BasicService.getUniqueInstance().getValue(this.inputData.spacerSize, this.spacerSize);
-    }
+   protected inputDataChanged() {
+      this.spacerSize = BasicService.getUniqueInstance().getValue(this.inputData.spacerSize, this.spacerSize);
+   }
 }
