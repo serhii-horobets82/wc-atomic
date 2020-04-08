f70ba2b729fb9aa9ee4c0c0882d58cd4543bd647
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-04-08 12:12:08 +0200

    modified

--- source/_showcase/page-get-started.ts
+++ source/_showcase/page-get-started.ts
@@ -1,6 +1,7 @@
 import { customElement, html, TemplateResult } from 'lit-element';
 import { PageAbstract } from './page-abstract';
 import { TypographyInputData, TypographyType } from '../typography/component';
+import {ColorType} from "..";
 
 @customElement('page-get-started')
 export class PageGetStarted extends PageAbstract {
@@ -9,11 +10,12 @@ export class PageGetStarted extends PageAbstract {
          <component-flex-container .containerClazzes="${["CONTAINER_50"]}" itemFlexBasisValue="100%">
             <component-spacer clazz="maxPaddingTop"></component-spacer>
             <component-typography .typographyType="${TypographyType.H2}" text="1">Base Project</component-typography>
+            <component-color colorType="${ColorType.BACKGROUND_COLOR}">
             <component-typography>
                clone base template from github to start your client app<component-link href="https://github.com/domoskanonos/wc-atomic-template"
                   >wc-atomic-template</component-link
                >
-            </component-typography>
+            </component-typography></component-color>
          </component-flex-container>
       `;
    }
